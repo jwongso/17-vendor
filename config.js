8dbd3fc5840bc3922095c8e74a659068160a2b58
wwon <jsaputra@riseup.net>
2026-05-08 19:52:44 +1200

    Default frontend config to Cloudflare backend

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.BOOKING_CONFIG = Object.assign({
   buildHash: '__HASH__',
-  bookedEndpoint: '/api/booked',
-  bookingEndpoint: '/api/booking',
+  bookedEndpoint: 'https://bookingbooth.pages.dev/api/booked',
+  bookingEndpoint: 'https://bookingbooth.pages.dev/api/booking',
   turnstileSiteKey: '__TURNSTILE_SITE_KEY__'
 }, window.BOOKING_CONFIG || {});
